3bac63bf52858b755744c642a535d6164281680b
Pedro Pinho <pedrohsistemas@outlook.com>
2025-12-23 19:37:27 -0300

    Release version 0.3.0

--- simple_python_rbac/__init__.py
+++ simple_python_rbac/__init__.py
@@ -4,4 +4,4 @@ from .exceptions import PermissionError
 
 __all__ = ["RBACManager", "RoleConfig", "Permissions", "get_all_permissions", "PermissionError"]
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
